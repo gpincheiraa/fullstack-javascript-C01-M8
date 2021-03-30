2f446a446bb7cb28c73184f36014ce8cf90e9b4b
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2021-03-30 17:48:37 -0300

    fix(backend-routes): fix backend routes adding prefix and set default route on server.js file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 const products = require("../controllers/products");
 
-router.route("/products").get(products.index)
+router.route("/api/products")
+  .get(products.index)
 
-// default route
-router.get("*", (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
-});
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = require("./routes");
+const apiRouter = require("./routes");
 const cors = require("cors");
 const authMiddleware = require("./auth")
 
 const app = express();
-
 
 // TODO: Intentar quitar cors que corriendo en local y en prod back y front se comuniquen sin problemas de cors
 app.use(
@@ -20,8 +19,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/products", authMiddleware);
-app.use("/", router);
+app.use("/api/*", authMiddleware);
+app.use("/", apiRouter);
+
+// default route
+apiRouter.get("*", (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
 
 const port = process.env.PORT || 3000;
 const environment = JSON.stringify(process.env.NODE_ENV);
